5c8afc010424e00337693a44ff7a4bbcd18d0bdd
Patrik Šimunič <patriksimunic@hotmail.com>
2017-02-09 16:49:48 +0100

    init push

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Patrik Šimunič</title>
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.min.css" />
+        <link rel="stylesheet" href="css/simunic.css" />
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.min.js"></script>
+        <script src="js/simunic.js"></script>
+    </head>
+    <body>
+        <nav>
+
+        </nav>
+        <article>
+            <div class="ui text container">
+            <div class="ui inverted segment">
+            <i class="cubes icon"></i>Under construction...
+            </div>
+            </div>
+        </article>
+        <footer>
+
+        </footer>
+    </body>
+</html>
