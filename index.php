b08ed1ef4fc04f2dc21d75c096360d344f6ff7f0
Patrik Šimunič <patriksimunic@hotmail.com>
2017-02-09 19:28:19 +0100

    contact

--- index.php
+++ index.php
@@ -11,10 +11,13 @@
         <script src="js/simunic.js"></script>
     </head>
     <body>
-        <nav>
-
+        <nav class="ui segment" id="nav">
+            <div class="ui secondary pointing menu">
+                <a class="item active" href="#services">Services</a>
+                <a class="item" href="#contact">Contact</a>
+            </div>
         </nav>
-        <article>
+        <article id="article">
             <div class="ui text container">
             <div class="ui inverted segment">
             <h2 class="ui header">
@@ -23,8 +26,37 @@
             </div>
             </div>
         </article>
-        <footer>
-
+        <footer class="ui inverted segment" id="footer">
+            <div class="ui text container">
+                <p>
+                    Patrik Šimunič
+                </p>
+                <h4 class="ui horizontal divider inverted header">
+                    <i class="call icon"></i> Contact
+                </h4>
+                <div class="ui grid">
+                    <div class="equal width row">
+                        <div class="column">
+                            IČ CZ...<br>
+                            Č.ú. 219349311/0300<br>
+                            Pod Babykou 277,<br>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Vsetín 755 01,<br>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Czech republic
+                        </div>
+                        <div class="column">
+                            <a href="mailto:patriksimunic@hotmail.com">patriksimunic@hotmail.com</a><br>
+                            <a href="tel:+420775175758">+420 775 175 758</a><br>
+                            <a href="https://github.com/7h3w4rd0c70r"><i class="github icon"></i> Github</a><br>
+                            <a href="https://linkedin.com/in/patriksimunic"><i class="linkedin icon"></i> LinkedIn</a>
+                        </div>
+                        <div class="column">
+                            <a href="https://twitter.com/7h3w4rd0c70r"><i class="twitter icon"></i> Twitter</a><br>
+                            <a href="skype:patriksimunic@hotmail.com?userinfo"><i class="skype icon"></i> Skype</a><br>
+                            <a href="sip:patrik.simunic@studentstc.cz"><i class="skype icon"></i> SfB</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </footer>
     </body>
 </html>
